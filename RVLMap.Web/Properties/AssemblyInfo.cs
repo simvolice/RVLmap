79f4e9cf4901eba2ca75720ddb4c48d39d2a8daa
simvolice <simvolice@gmail.com>
2014-08-17 21:38:31 +0600

    Связь создана в виде WCF службы, DomainService работает только с локальными базами

--- RVLMap.Web/Properties/AssemblyInfo.cs
+++ RVLMap.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных с этой сборкой.
-[assembly: AssemblyTitle("MyPress.Web")]
+[assembly: AssemblyTitle("RVLMap.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyPress.Web")]
+[assembly: AssemblyProduct("RVLMap.Web")]
 [assembly: AssemblyCopyright("© , 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
-[assembly: Guid("8c32523d-eb4b-4e7a-a8b8-459521ac0add")]
+[assembly: Guid("e7859983-87cd-472e-a92d-37980286b6a3")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
---
+++ RVLMap.Web/Service1.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RVLMap.Web
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
+    public class Service1 : IService1
+    {
+
+
+
+
+
+
+        base_vetlabEntities cd = new base_vetlabEntities();
+
+
+        public void DoWork()
+        {
+       
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
